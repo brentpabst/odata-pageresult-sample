d308bb7c4ac878873e3acdd9aa14f548fee9db20
Brent Pabst <brent.pabst@dudesolutions.com>
2015-02-26 08:11:05 -0400

    Sample import

---
+++ OData/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData;
+
+namespace OData.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        readonly string[] _samples = { "value1", "value2", "more", "more", "more", "more", "more", "more" };
+
+        // GET api/values
+        [EnableQuery]
+        public PageResult<string> GetFeed()
+        {
+            return new PageResult<string>(_samples, null, _samples.Count());
+        }
+    }
+}
